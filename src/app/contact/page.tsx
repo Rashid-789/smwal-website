c9e68d657845b061c5e20c7bf1e4caed40786baf
rashid dev <rashidiyaoo7@gmail.com>
2026-04-13 15:02:29 +0500

    code updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,6 @@
 import ContactHero from "@/features/contact/sections/ContactHero";
 import Footer from "@/components/site/Footer";
-
+//page for contact us page
 export default function ContactPage() {
   return (
     <><ContactHero />
